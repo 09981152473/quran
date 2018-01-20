f499fd68bd1d04224da9a54e30cfe63d01a2c4f9
mehran motiee <straxico@gmail.com>
2018-01-20 23:15:32 +0330

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 import xmltodict
 import sys
 import time
+import random
 
 
 ar=requests.get("https://tyto.ir/quran-simple.xml")
@@ -21,7 +22,10 @@ for i in range(0,114):
     surename=fa['quran']['sura'][i]['@name']
     list.append([surename])
     listnum[surename]=i
-startmarkup = ReplyKeyboardMarkup(keyboard=list)
+list.append(['back'])
+suremarkup = ReplyKeyboardMarkup(keyboard=list)
+startmarkup=ReplyKeyboardMarkup(keyboard=[['list'],['random']])
+
 class MessageCounter(telepot.aio.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(MessageCounter, self).__init__(*args, **kwargs)
@@ -30,14 +34,30 @@ class MessageCounter(telepot.aio.helper.ChatHandler):
     async def on_chat_message(self, msg):
         self._command=msg['text']
         if self._command=='/start':
-          self._wel='welcome'
+          self._wel='wellcome'
           await self.sender.sendMessage(self._wel,reply_markup=startmarkup )
-          
+        if self._command=='list':
+          self._wel='list'
+          await self.sender.sendMessage(self._wel,reply_markup=suremarkup )
+        if self._command=='random':
+              self._command=random.randint(0,113)
+              self._d=fa['quran']['sura'][self._command]['aya']
+              self._i=random.randint(0,self._d)
+              self._text=''
+              self._arr=ar['quran']['sura'][self._sure]['aya'][self._i]['@text']
+              self._enn=en['quran']['sura'][self._sure]['aya'][self._i]['@text']
+              self._faa=fa['quran']['sura'][self._sure]['aya'][self._i]['@text']
+              self._text='....#'+self._surename+'....'+str(self._i+1)+"...."+'\n'
+              self._text+='\n'+self._arr+'\n'+self._enn+'\n'+self._faa
+              self._text+='\n'+ self._command+":"+self._i
+              await self.sender.sendMessage(str(self._text))
+            
         if self._command in listnum.keys():
            self._command=listnum[self._command]
-           self._sure=int(self._command)-1
+           self._sure=int(self._command)
            self._surename=fa['quran']['sura'][self._sure]['@name']
            self._d=fa['quran']['sura'][self._sure]['aya']
+           
            for self._i in range(len(self._d)):
               self._text=''
               self._arr=ar['quran']['sura'][self._sure]['aya'][self._i]['@text']
@@ -47,14 +67,18 @@ class MessageCounter(telepot.aio.helper.ChatHandler):
               self._text+='\n'+self._arr+'\n'+self._enn+'\n'+self._faa
               self._text+='\n'
               if len(self._text)<4000:
-                await self.sender.sendMessage(self._text)
+                  try:
+                     await self.sender.sendMessage(str(self._text))
+                     
+                  except telepot.exception.TelegramError as ex:
+                     print(self._i)
+                     time.sleep(31)
+                     await self.sender.sendMessage(str(self._text))
+
               else:
                 self._text='too big'
                 print('too big')
                 await self.sender.sendMessage(self._text )
-        
-    
-
 
 app = Flask(__name__)
 
